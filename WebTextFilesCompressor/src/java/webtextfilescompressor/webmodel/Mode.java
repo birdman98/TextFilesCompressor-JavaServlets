c0abc7e23f45271717a1204e7f1752205a8fcf5a
birdman98 <piotrmatras21@gmail.com>
2019-12-21 10:38:28 +0100

    added Mode enum

---
+++ WebTextFilesCompressor/src/java/webtextfilescompressor/webmodel/Mode.java
@@ -0,0 +1,12 @@
+package webtextfilescompressor.webmodel;
+
+/**
+ *
+ * @author Piotr Matras
+ * @version 1.0
+ * Possible modes of working with file compressor
+ */
+public enum Mode {
+    COMPRESS,
+    DECOMPRESS    
+}
