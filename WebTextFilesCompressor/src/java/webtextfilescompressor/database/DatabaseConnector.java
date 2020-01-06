5aec98610b58b6d902850a8bd65654b2b147ff9f
birdman98 <piotrmatras21@gmail.com>
2020-01-06 18:07:59 +0100

    added making connection with database

--- WebTextFilesCompressor/src/java/webtextfilescompressor/database/DatabaseConnector.java
+++ WebTextFilesCompressor/src/java/webtextfilescompressor/database/DatabaseConnector.java
@@ -1,10 +1,51 @@
 package webtextfilescompressor.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Piotr Matras
  * @version 1.0
  */
 public class DatabaseConnector {
+    
+    private final String databaseUsername = "dev";
+    private final String databasePassword = "dev";
+    private final String databaseURL = "jdbc:derby://localhost:1527/OperationsHistory";
+    private final String tableName = "OperationsHistory";
+    
+    private static Connection connection = null;
+    
+    public DatabaseConnector() {
+         if(connection == null) {
+             connectWithDatabase();         
+         }        
+    }
+    
+    private boolean loadJDBCDriver() {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver"); 
+            return true;
+        } catch(ClassNotFoundException e) {
+            System.err.println("Failed to load JDBC driver, reason: " + e.getMessage());
+        }
+        
+        return false;               
+    }
+    
+    private void connectWithDatabase() {
+        if(!loadJDBCDriver()) {
+            System.err.println("Failed to load JDBC driver!");
+            return;
+        }
+        
+        try {
+            connection = DriverManager.getConnection(databaseURL, databaseUsername, databasePassword);
+        } catch(SQLException e) {
+            System.err.println("Failed to get connection with database, reason: " + e.getMessage());
+        }   
+    }
     
 }
