16dd2a796188ec23fa3f6ae36870565efc026dcb
birdman98 <piotrmatras21@gmail.com>
2020-01-06 19:52:03 +0100

    added javadocs

--- WebTextFilesCompressor/src/java/webtextfilescompressor/database/DatabaseConnector.java
+++ WebTextFilesCompressor/src/java/webtextfilescompressor/database/DatabaseConnector.java
@@ -10,19 +10,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * Class for realising connection with database and operations on database
  * @author Piotr Matras
  * @version 1.0
  */
 public class DatabaseConnector {
     
+    /**
+     * databaseUsername - name of user to connect with database
+     */
     private final String databaseUsername = "dev";
+    /**
+     * databasePassword - password of user to connect with database
+     */
     private final String databasePassword = "dev";
+    /**
+     * databaseURL - URL to connect with database
+     */
     private final String databaseURL = "jdbc:derby://localhost:1527/OperationsHistory";
+    /**
+     * tableName - name of table in database
+     */
     private final String tableName = "OperationsHistory";
     
+    /**
+     * connection - object of Connection class, which represents connection with database
+     */
     private static Connection connection = null;
     
+    /**
+     * Makes new connection with database if already not connected and tries to create table, if not exists
+     */
     public DatabaseConnector() {
          if(connection == null) {
              connectWithDatabase();  
@@ -30,6 +48,10 @@ public class DatabaseConnector {
          }        
     }
     
+    /**
+     * Loads the JDBC driver
+     * @return true if loading was successful, else false
+     */
     private boolean loadJDBCDriver() {
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver"); 
@@ -41,6 +63,9 @@ public class DatabaseConnector {
         return false;               
     }
     
+    /**
+     * Makes connection with database if JDBC driver was loaded successfully
+     */    
     private void connectWithDatabase() {
         if(!loadJDBCDriver()) {
             System.err.println("Failed to load JDBC driver!");
@@ -54,6 +79,9 @@ public class DatabaseConnector {
         }   
     }
     
+    /**
+     * Creates table in database if already not exists
+     */
     private void createTable() {
         try {
             Statement statement = connection.createStatement();   
@@ -65,6 +93,13 @@ public class DatabaseConnector {
         }        
     }
     
+    /**
+     * Inserts data into table in database
+     * @param mode - mode of file compressor
+     * @param inputFile - input file path
+     * @param outputFile - output file path
+     * @return true if data inserted successfuly, else false
+     */
     public boolean insertData(final String mode, final String inputFile, final String outputFile) {
         try {
             PreparedStatement preparedStatement = this.connection.prepareStatement("INSERT INTO " + this.tableName + " VALUES (?, ?, ?)");
@@ -82,6 +117,10 @@ public class DatabaseConnector {
         return false;
     }
     
+    /**
+     * Gets all records from table in database and parses it into collection of data
+     * @return List of all records in table in database
+     */
     public List<List<String>> getOperationsHistory() {
         List<List<String>> operationsHistory = new ArrayList<List<String>>();
         try {
